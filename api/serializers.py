e039fa8e225f4323f47676f16009bf54a9c6f89f
jhonj <johnvega@wposs.com>
2021-06-07 16:26:30 -0500

    crud user

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,11 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', "first_name", "last_name")
+        fields = ("id", 'username', 'email', "first_name", "last_name", "password")
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id': {'read_only': True}
+        }
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- api/views.py
+++ api/views.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.models import User, Group
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 from rest_framework import generics, permissions
+from rest_framework.viewsets import ModelViewSet
 
 from api.serializers import UserSerializer, GroupSerializer
 
 
-class UserList(generics.ListCreateAPIView):
+class UserView(ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
 
-class UserDetails(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
 
 
 class GroupList(generics.ListAPIView):
